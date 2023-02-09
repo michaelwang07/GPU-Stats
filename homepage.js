fd40d091e49705de7ab5998f1b7c172eb2e67b7d
michaelwang07 <michaelwang07@yahoo.com>
2023-02-09 00:35:15 -0800

    table sorting update 1

--- homepage.js
+++ homepage.js
@@ -10,3 +10,18 @@ function myFunction() {
       y.style.display = "block";
     }
 }
+
+function sortTableByColumn(table, column, asc = true) {
+  const dirModifier = asc ? 1 : -1;
+  const tBody = table.tBodies[0];
+  const rows = Array.from(tBody.querySelectorAll("tr"));
+
+  //Sort each row
+  const sortedRows = rows.sort((a, b) => {
+    console.log(a);
+    console.log(b);
+  });
+}
+
+//table-sortable || table_sortable
+sortTableByColumn(document.getElementById("table_sortable"), 1);
